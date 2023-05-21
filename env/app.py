36b36a421918c90e4a79b74604c36c80af270b31
dynamic2code <dynamicdesingns@gmail.com>
2023-05-21 10:30:22 +0300

    pages set up and connection 1

--- env/app.py
+++ env/app.py
@@ -9,18 +9,24 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
-
+#pass
 @app.route('/pass_login')
 def pass_login():
-    return 'Hello, World!'
+    return render_template('pass_login.html')
 
+@app.route('/pass_login/pass_main')
+def pass_main():
+    return render_template('pass_main.html')
+
+# admin
 @app.route('/admin_login')
 def admin_login():
-    return 'Hello, World!'
+    return render_template('admin_login.html')
 
+# driver
 @app.route('/drivers_login')
-def drivers_login():
-    return 'Hello, World!'
+def driver_login():
+    return render_template('driver_login.html')
 
 if __name__ == "__main__":
     app.run()
