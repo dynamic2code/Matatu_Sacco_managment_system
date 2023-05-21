5e649c3fa1c184336a3d299c2449127c404c2d8f
dynamic2code <dynamicdesingns@gmail.com>
2023-05-21 10:40:31 +0300

    point upto login page working

--- env/app.py
+++ env/app.py
@@ -18,15 +18,24 @@ def pass_login():
 def pass_main():
     return render_template('pass_main.html')
 
+# driver
+@app.route('/driver_login')
+def driver_login():
+    return render_template('driver_login.html')
+
+@app.route('/driver_login/driver_main')
+def driver_main():
+    return render_template('driver_main.html')
+
 # admin
 @app.route('/admin_login')
 def admin_login():
     return render_template('admin_login.html')
 
-# driver
-@app.route('/drivers_login')
-def driver_login():
-    return render_template('driver_login.html')
+@app.route('/admin_login/admin_main')
+def admin_main():
+    return render_template('admin_main.html')
+
 
 if __name__ == "__main__":
     app.run()
