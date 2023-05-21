854a688807d1c6710adddeab1f8813d714cb4bd7
dynamic2code <dynamicdesingns@gmail.com>
2023-05-21 11:33:46 +0300

    addition of register page, working point till reg

--- env/app.py
+++ env/app.py
@@ -9,21 +9,38 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
+
 #pass
+@app.route('/pass_register')
+def pass_register():
+    return render_template('pass_register.html')
+
+@app.route('/pass_register/pass_registerd', methods=['POST'])
+def pass_registerd():
+    return render_template('pass_main.html')
+
 @app.route('/pass_login')
 def pass_login():
     return render_template('pass_login.html')
 
-@app.route('/pass_login/pass_main')
+@app.route('/pass_login/pass_main', methods=['POST'])
 def pass_main():
     return render_template('pass_main.html')
 
 # driver
+@app.route('/driver_register')
+def driver_register():
+    return render_template('driver_register.html')
+
+@app.route('/driver_register/driver_registerd', methods=['POST'])
+def driver_registerd():
+    return render_template('driver_main.html')
+
 @app.route('/driver_login')
 def driver_login():
     return render_template('driver_login.html')
 
-@app.route('/driver_login/driver_main')
+@app.route('/driver_login/driver_main', methods=['POST'])
 def driver_main():
     return render_template('driver_main.html')
 
@@ -32,7 +49,7 @@ def driver_main():
 def admin_login():
     return render_template('admin_login.html')
 
-@app.route('/admin_login/admin_main')
+@app.route('/admin_login/admin_main', methods=['POST'])
 def admin_main():
     return render_template('admin_main.html')
 
